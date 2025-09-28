8550b5d561a03e5acac96684e9a0a372a4e2b546
Tijil Sharma <sharmatijil06@gmail.com>
2025-09-28 14:28:07 +0530

    Adding auth

--- routes/chart.routes.js
+++ routes/chart.routes.js
@@ -3,7 +3,7 @@ import {authMiddleware} from "../middlewares/confirmjwtToken.js";
 import {getChart, getChartLine} from "../controllers/chart.controller.js";
 
 const router = Router();
-router.get('/pie/:id',getChart);
+router.get('/pie/:id',authMiddleware,getChart);
 router.get('/line/:id',getChartLine);
 
 export default router;
