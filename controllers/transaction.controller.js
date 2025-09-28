d8fcb867d4d1247003765399639e2665760d42e0
Tijil Sharma <sharmatijil06@gmail.com>
2025-09-28 11:09:06 +0530

    error

--- controllers/transaction.controller.js
+++ controllers/transaction.controller.js
@@ -18,7 +18,7 @@ export const transactionController = async (req, res) => {
         }
         catch(err){
             console.error('Transaction save error:', err);
-            res.status(400).json({ error: err.message || err });
+        res.status(400).json({ error: err.message || err },{vendor});
         }
 
 
